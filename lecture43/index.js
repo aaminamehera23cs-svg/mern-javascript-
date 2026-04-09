4a58064547c50487e67462be678f2e05cd872083
aaminamehera23cs-svg <aaminamehera23cs@rnsit.ac.in>
2026-04-09 14:09:53 +0530

    Add lecture43 work

---
+++ lecture43/index.js
@@ -0,0 +1,23 @@
+function printName(){
+    console.log("Sam Altman");
+}
+printName();
+
+function Count(){
+    for(let i=1;i<=10;i++){
+        console.log(i);
+    }
+}
+Count();
+
+function average(num1,num2){
+    let avg=(num1+num2)/2;
+    console.log("average",avg);
+}
+average(10,20);
+function getSum(a,b,c){
+    let sum=a+b+c;
+    return sum;
+}
+console.log(getSum(10,20,30));
+
